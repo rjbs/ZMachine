879aeec8743c8537e59cf021844c3017bb212ce1
Ricardo Signes <rjbs@cpan.org>
2013-01-18 22:43:18 -0500

    document all methods

--- lib/ZSCII/Codec.pm
+++ lib/ZSCII/Codec.pm
@@ -21,6 +21,18 @@ be mapped to the Unicode "EM SPACE" character.
 At present, a text string can be encoded to a packed Z-character string, but
 not to an array of ZSCII values.
 
+# Unicode text
+# |        ^
+# v        |
+# ZSCII text
+# |        ^
+# v        |
+# Z-characters
+# |        ^
+# v        |
+# packed Z-characters
+
+
 =cut
 
 =method new
@@ -55,23 +67,17 @@ sub new {
 
 =method encode
 
-  my $packed_zchars = $z->encode( $text );
+  my $packed_zchars = $z->encode( $unicode_text );
 
 This method takes a string of text and encodes it to a bytestring of packed
 Z-characters.
 
-=cut
+Internally, it converts the Unicode text to ZSCII, then to Z-characters, and
+then packs them.  Before this processing, any native newline characters (the
+value of C<\n>) are converted to C<U+000D> to match the Z-Machine's use of
+character 0x00D for newline.
 
-# Unicode text
-# |        ^
-# v        |
-# ZSCII text
-# |        ^
-# v        |
-# Z-characters
-# |        ^
-# v        |
-# packed Z-characters
+=cut
 
 sub encode {
   my ($self, $string) = @_;
@@ -91,12 +97,16 @@ sub encode {
 This method takes a bytestring of packed Z-characters and returns a string of
 text.
 
+Internally, it unpacks the Z-characters, converts them to ZSCII, and then
+converts those to Unicode.  Any ZSCII characters 0x00D are converted to the
+value of C<\n>.
+
 =cut
 
 sub decode {
   my ($self, $bytestring) = @_;
 
-  my $zchars  = $self->unpack_zchars( $bytestring);
+  my $zchars  = $self->unpack_zchars( $bytestring );
   my $zscii   = $self->zchars_to_zscii( $zchars );
   my $unicode = $self->zscii_to_unicode( $zscii );
 
@@ -119,25 +129,6 @@ my %ZSCII_FOR = (
 
 my %UNICODE_FOR = reverse %ZSCII_FOR;
 
-sub unicode_to_zscii {
-  my ($self, $unicode_text) = @_;
-
-  my $zscii = '';
-  for (0 .. length($unicode_text) - 1) {
-    my $char = substr $unicode_text, $_, 1;
-
-    Carp::croak(
-      sprintf "no ZSCII character available for Unicode U+%v05X <%s>",
-        $char,
-        charnames::viacode(ord $char),
-    ) unless my $zscii_char = $ZSCII_FOR{ $char };
-
-    $zscii .= $zscii_char;
-  }
-
-  return $zscii;
-}
-
 # We can use these characters below because they all (save for the magic A2-C6)
 # are the same in Unicode/ASCII/ZSCII. -- rjbs, 2013-01-18
 my @ALPHABETS = (
@@ -159,6 +150,79 @@ for my $i (0 .. 2) {
   }
 }
 
+=method unicode_to_zscii
+
+  my $zscii_string = $z->unicode_to_zscii( $unicode_string );
+
+This method converts a Unicode string to a ZSCII string, using the dialect of
+ZSCII for the ZSCII::Codec's configuration.
+
+If the Unicode input contains any characters that cannot be mapped to ZSCII, an
+exception is raised.
+
+=cut
+
+sub unicode_to_zscii {
+  my ($self, $unicode_text) = @_;
+
+  my $zscii = '';
+  for (0 .. length($unicode_text) - 1) {
+    my $char = substr $unicode_text, $_, 1;
+
+    Carp::croak(
+      sprintf "no ZSCII character available for Unicode U+%v05X <%s>",
+        $char,
+        charnames::viacode(ord $char),
+    ) unless my $zscii_char = $ZSCII_FOR{ $char };
+
+    $zscii .= $zscii_char;
+  }
+
+  return $zscii;
+}
+
+=method zscii_to_unicode
+
+  my $unicode_string = $z->zscii_to_unicode( $zscii_string );
+
+This method converts a ZSCII string to a Unicode string, using the dialect of
+ZSCII for the ZSCII::Codec's configuration.
+
+If the ZSCII input contains any characters that cannot be mapped to Unicode, an
+exception is raised.  I<In the future, it may be possible to request a Unicode
+replacement character instead.>
+
+=cut
+
+sub zscii_to_unicode {
+  my ($self, $zscii) = @_;
+
+  my $unicode = '';
+  for (0 .. length($zscii) - 1) {
+    my $char = substr $zscii, $_, 1;
+
+    Carp::croak(
+      sprintf "no Unicode character available for ZSCII %#v05x", $char,
+    ) unless my $unicode_char = $UNICODE_FOR{ $char };
+
+    $unicode .= $unicode_char;
+  }
+
+  return $unicode;
+}
+
+=method zscii_to_zchars
+
+  my $zchars = $z->zscii_to_zchars( $zscii_string );
+
+Given a string of ZSCII characters, this method will return a (unpacked) string
+of Z-characters.
+
+It will raise an exception on ZSCII codepoints that cannot be represented as
+Z-characters, which should not be possible with legal ZSCII.
+
+=cut
+
 sub zscii_to_zchars {
   my ($self, $zscii) = @_;
 
@@ -172,18 +236,84 @@ sub zscii_to_zchars {
       next;
     }
 
-    Carp::croak(
-      sprintf "can't encode ZSCII codepoint %#v05x in Z-characters",
-        $zscii_char
-    );
+    $zchars = "\x05\x06"; # The escape code for a ten-bit ZSCII character.
+    my $ord = ord $zscii;
+
+    if ($ord >= 1024) {
+      Carp::croak(
+        sprintf "can't encode ZSCII codepoint %#v05x in Z-characters",
+          $zscii_char
+      );
+    }
+
+    my $top = ($ord & 0b1111100000) >> 5;
+    my $bot = ($ord & 0b0000011111);
+
+    $zchars .= chr($top) . chr($bot);
   }
 
   return $zchars;
 }
 
+=method zchars_to_zscii
+
+  my $zscii = $z->zchars_to_zscii( $zchars_string );
+
+Given a string of (unpacked) Z-characters, this method will return a string of
+ZSCII characters.
+
+It will raise an exception when the right thing to do can't be determined.
+Right now, that could mean lots of things.
+
+=cut
+
+sub zchars_to_zscii {
+  my ($self, $zchars) = @_;
+
+  my $text = '';
+  my $alphabet = 0;
+
+  # We copy to avoid destroying our input.  That's just good manners.
+  # -- rjbs, 2013-01-18
+  while (length $zchars) {
+    my $char = substr $zchars, 0, 1, '';
+
+    last unless defined $char; # needed because of redo below
+
+    my $ord = ord $char;
+
+    if ($ord eq 0) { $text .= q{ }; next; }
+
+    if    ($ord == 0x04) { $alphabet = 1; redo }
+    elsif ($ord == 0x05) { $alphabet = 2; redo }
+
+    if ($alphabet == 2 && $ord == 0x06) {
+      my $next_two = substr $zchars, 0, 2, '';
+      Carp::croak("ten-bit ZSCII encoding segment terminated early")
+        unless length $next_two == 2;
+
+      Carp::croak("ten-bit ZSCII encoding not yet supported"); # TODO
+    }
+
+    if ($ord >= 0x06 && $ord <= 0x1F) {
+      $text .= $ALPHABETS[ $alphabet ][ $ord - 6 ];
+      $alphabet = 0;
+      next;
+    }
+
+    Carp::croak("unknown zchar <$char> encountered in alphabet <$alphabet>");
+  }
+
+  return $text;
+}
+
 =method pack_zchars
 
   my $packed_zchars = $z->pack_zchars( $zchars_string );
+
+This method takes a string of unpacked Z-characters and packs them into a
+bytestring with three Z-characters per word.  The final word will have its top
+bit set.
 
 =cut
 
@@ -206,6 +336,19 @@ sub pack_zchars {
 
   return $bytestring;
 }
+
+=method unpack_zchars
+
+  my $zchars_string = $z->pack_zchars( $packed_zchars );
+
+Given a bytestring of packed Z-characters, this method will unpack them into a
+string of unpacked Z-characters that aren't packed anymore because they're
+unpacked instead of packed.
+
+Exceptions are raised if the input bytestring isn't made of an even number of
+octets, or if the string continues past the first word with its top bit set.
+
+=cut
 
 sub unpack_zchars {
   my ($self, $bytestring) = @_;
@@ -230,59 +373,6 @@ sub unpack_zchars {
   }
 
   return $zchars;
-}
-
-sub zchars_to_zscii {
-  my ($self, $zchars) = @_;
-
-  my $text = '';
-  my $alphabet = 0;
-
-  # We copy to avoid destroying our input.  That's just good manners.
-  # -- rjbs, 2013-01-18
-  while (length $zchars) {
-    my $char = substr $zchars, 0, 1, '';
-
-    last unless defined $char; # needed because of redo below
-
-    my $ord = ord $char;
-
-    if ($ord eq 0) { $text .= q{ }; next; }
-
-    if    ($ord == 0x04) { $alphabet = 1; redo }
-    elsif ($ord == 0x05) { $alphabet = 2; redo }
-
-    if ($alphabet == 2 && $ord == 0x06) {
-      Carp::croak("ten-bit ZSCII characters not yet implemented");
-    }
-
-    if ($ord >= 0x06 && $ord <= 0x1F) {
-      $text .= $ALPHABETS[ $alphabet ][ $ord - 6 ];
-      $alphabet = 0;
-      next;
-    }
-
-    Carp::croak("unknown zchar <$char> encountered in alphabet <$alphabet>");
-  }
-
-  return $text;
-}
-
-sub zscii_to_unicode {
-  my ($self, $zscii) = @_;
-
-  my $unicode = '';
-  for (0 .. length($zscii) - 1) {
-    my $char = substr $zscii, $_, 1;
-
-    Carp::croak(
-      sprintf "no Unicode character available for ZSCII %#v05x", $char,
-    ) unless my $unicode_char = $UNICODE_FOR{ $char };
-
-    $unicode .= $unicode_char;
-  }
-
-  return $unicode;
 }
 
 1;
